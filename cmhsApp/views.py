007ee8dc992862575e25fd9e22ed67be83b86689
Flora72 <zahratheo569@gmail.com>
2025-12-20 02:11:39 +0300

    updated to match the files in accounts/templated

--- cmhsApp/views.py
+++ cmhsApp/views.py
@@ -16,7 +16,7 @@ def login(request):
     return render(request,'login.html')
 
 def signup(request):
-    return render(request,'signup.html')
+    return render(request, 'accounts/register.html')
 
 # dashboard views
 def patient_dashboard(request):
