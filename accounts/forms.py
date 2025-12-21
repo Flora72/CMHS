27655c02916e95a2c037b11c2aeb92eb4548d72f
Flora72 <zahratheo569@gmail.com>
2025-12-21 13:12:18 +0300

    loop to ensure tailwind css is applied to all placeholders

--- accounts/forms.py
+++ accounts/forms.py
@@ -9,11 +9,11 @@ class PatientRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'phone_number', 'role']
-        widgets = {
-            'username': forms.TextInput(attrs={
-                'class': 'w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-chiromo-gold'}),
-            'email': forms.EmailInput(attrs={
-                'class': 'w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-chiromo-gold'}),
-            'phone_number': forms.TextInput(attrs={
-                'class': 'w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-chiromo-gold'}),
-        }
+
+    def __init__(self, *args, **kwargs):
+        super(PatientRegistrationForm, self).__init__(*args, **kwargs)
+
+
+        for field_name in self.fields:
+            self.fields[field_name].widget.attrs[
+                'class'] = 'w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-chiromo-gold'
