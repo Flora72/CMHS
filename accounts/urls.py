b20dcf973e09b28aa6a696b04cea037564f4a3f7
Flora72 <zahratheo569@gmail.com>
2025-12-24 12:33:02 +0300

    Register URL route for therapist dashboard

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
+    path('therapist-dashboard/', views.therapist_dashboard, name='therapist_dashboard'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('logout/', LogoutView.as_view(next_page='home'), name='logout')
 
