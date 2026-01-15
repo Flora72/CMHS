5f7bce1e50289bef0484decee4006b2e627eaea3
Flora72 <zahratheo569@gmail.com>
2026-01-15 23:49:08 +0300

    routing of the action buttons approve and decline, settings

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,9 +8,11 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('therapist-dashboard/appointments/', views.therapist_appointments, name='therapist_appointments'),
     path('therapist-dashboard/patients/', views.therapist_patients, name='therapist_patients'),
-
+    path('appointments/approve/<int:pk>/', views.approve_appointment, name='approve_appointment'),
+    path('appointment/decline/<int:pk>/', views.decline_appointment, name='decline_appointment'),
     path('therapist-dashboard/', views.therapist_dashboard, name='therapist_dashboard'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('settings/', views.settings_view, name='settings'),
     path('logout/', LogoutView.as_view(next_page='home'), name='logout')
 
 ]
