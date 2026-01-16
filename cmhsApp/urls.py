bc5c2566b206169eec0c1b49cff111040c55070e
Flora72 <zahratheo569@gmail.com>
2026-01-16 14:43:05 +0300

    Update main URL routing to include auth views.

--- cmhsApp/urls.py
+++ cmhsApp/urls.py
@@ -1,6 +1,6 @@
 from . import views
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.index, name='index'),
     path('', views.about, name='about'),
@@ -9,5 +9,13 @@ urlpatterns = [
     path('', views.signup, name='signup'),
     path('', views.patient_dashboard, name='patient_dashboard'),
     path('', views.therapist_dashboard, name='therapist_dashboard'),
+    path('password-change/', auth_views.PasswordChangeView.as_view(
+        template_name='accounts/password_change.html',
+        success_url='/password-change/done/'
+    ), name='change_password'),
+
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(
+        template_name='accounts/password_change_done.html'
+    ), name='password_change_done'),
 
 ]
