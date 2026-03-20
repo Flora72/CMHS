b2a967f0104f2192ceca73677db68078adf48433
Flora72 <zahratheo569@gmail.com>
2026-03-20 16:58:40 +0300

    email failing workaround

--- CMHS/settings.py
+++ CMHS/settings.py
@@ -140,7 +140,7 @@ AUTH_USER_MODEL = 'accounts.User'
 
 
 # EMAIL CONFIGURATION
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
 EMAIL_USE_TLS = False
