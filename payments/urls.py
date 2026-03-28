189d4df5f79787ea8c34f3278115b5deca0d05bc
Flora72 <zahratheo569@gmail.com>
2026-03-29 00:06:53 +0300

    routing (initiate payment)

--- payments/urls.py
+++ payments/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('pricing/', views.pricing_page, name='pricing_page'),
     path('pay/initiate/', views.initiate_payment, name='initiate_payment'),
-
+     path('pay/initiate/<int:appointment_id>/', views.initiate_payment, name='initiate_payment'),
     path('pay/success/', views.payment_success, name='payment_success'),
     path('payments/history/', views.transaction_history, name='transaction_history'),
     path('callback/', views.mpesa_callback, name='mpesa_callback'),
