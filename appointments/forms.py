141fa95b32b0e4113204eb77e0e494d88d1038d1
Flora72 <zahratheo569@gmail.com>
2026-04-17 15:37:02 +0300

    textfield to textarea attribute

--- appointments/forms.py
+++ appointments/forms.py
@@ -43,7 +43,7 @@ class BookingForm(forms.ModelForm):
             'mode': forms.Select(attrs={
                 'class': 'w-full px-4 py-3 border-2 border-gray-100 rounded-xl focus:border-chiromo-gold'
             }),
-            'notes': forms.TextField(attrs={
+            'notes': forms.TextArea(attrs={
                 'rows': 3,
                 'class': 'w-full px-4 py-3 border-2 border-gray-100 rounded-xl focus:border-chiromo-gold',
                 'placeholder': 'Briefly describe your reason for visit...'
